748baa8cc252cb09f05ecddc8b2e623dd1a669ec
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-05-27 17:30:03 -0600

    tailwind setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import People from '../components/People';
 
 export default function Home() {
   return (
-    <div className='home-div'>
+    <div className='flex flex-col items-center justify-center p-8'>
       <h1> First Next.js Page </h1>
+      <People />
     </div>
   )
   }
